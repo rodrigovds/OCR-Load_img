c86835052f8578723a1bfd440919da06711220e8
Rodrigo Mondini <47304763+rodrigovds@users.noreply.github.com>
2020-01-28 06:29:10 -0300

    Add files via upload
    Imagem a ser lida: c:\ocr_teste
    Na pasta bin criar uma pasta com nome tessdata (precisa ser este nome) e copiar o arquivo eng para lá;
    Necessário instalar o pacote Tesseract - no visual studio. (Nuget)

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testImage = @"C:\ocr_teste.png";
+            try
+            {
+                using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImage))
+
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var texto = page.GetText();
+                            Console.WriteLine("Taxa de precisão: {0}", page.GetMeanConfidence());
+                            Console.WriteLine("Texto : \r\n{0}", texto);
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: {0}", ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
